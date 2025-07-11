c0a0fcb087ec52b8dcfc03801a8cf9ca040af125
paveltorres <paveltorres2020@gmail.com>
2025-07-11 17:20:38 -0500

    mil mas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { registerRootComponent } from 'expo';
+/* import { registerRootComponent } from 'expo';
 
 import App from './App';
 import 'react-native-gesture-handler';
@@ -6,3 +6,32 @@ import 'react-native-gesture-handler';
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+ */
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import RickAndMortyCharacters from './lib/reckieMartinService'; // 👈 NUEVA IMPORTACIÓN
+
+const Stack = createNativeStackNavigator();
+
+import { SafeAreaView, StyleSheet } from 'react-native';
+import RickAndMortyCharacters from './lib/reckieMartinService';
+
+export default function Index() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen
+          name="RickAndMorty"
+          component={RickAndMortyCharacters}
+          options={{ title: 'Personajes Rick & Morty' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
